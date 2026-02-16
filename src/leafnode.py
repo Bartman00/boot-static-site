bc84bd178764db7def97dab51747826d09232e94
Brett Mattas <bmattas2@gmail.com>
2026-02-16 14:27:17 -0600

    Added LeafNode

---
+++ src/leafnode.py
@@ -0,0 +1,55 @@
+from htmlnode import HTMLNode
+
+class LeafNode(HTMLNode):
+
+    def __init__(self, tag, value, props=None):
+        
+        assert value is not None, "LeafNode value can't be None"
+        super().__init__(tag=tag, 
+                         value=value, 
+                         children=None, 
+                         props=props)
+    
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("leaf nodes require a value")
+
+        if self.tag is None:
+            return self.value
+
+        ret = f"<{self.tag}"
+        if self.props is not None:
+            ret += self.props_to_html()
+        ret += ">"
+
+        ret += self.value
+        ret += f"</{self.tag}>"
+
+        return ret
+
+    def __repr__(self):
+
+        ret = f"tag = {self.tag}"
+        ret += f"\nvalue = {self.value}"
+
+        if self.props is None:
+            ret += "\nprops = None"
+        else:
+            ret += "\n\nprops:\n"
+            for k, v in self.props.items():
+                ret += f"{k}: {v}\n"
+
+        return ret
+
+
+if __name__ == '__main__':
+    print('Inside leafnode.py')
+    leaf = LeafNode("a", "Click me!", 
+                 {"href": "https://www.google.com"})
+    print(leaf.to_html())
+
+    print(leaf)
+
+
+    leaf_2 = LeafNode("p", "This is a paragraph")
+    print(leaf_2.to_html())
---
+++ src/test_leafnode.py
@@ -0,0 +1,58 @@
+import unittest
+
+from leafnode import LeafNode
+
+class TestTextNode(unittest.TestCase):
+    def setUp(self) -> None:
+        self.node = LeafNode("<p>", "a value", 
+                             props = {'k1':'v1', 'k2':'v2'})
+
+    def test_eq(self):
+
+        self.assertEqual(self.node.tag, "<p>")
+        self.assertEqual(self.node.value, "a value")
+        self.assertEqual(self.node.props, {'k1':'v1', 'k2':'v2'})
+
+    def test_assert_bad_inputs(self):
+
+        with self.assertRaises(AssertionError):
+            LeafNode(1, 'a')
+        with self.assertRaises(AssertionError):
+            LeafNode('1', 2)
+        with self.assertRaises(AssertionError):
+            LeafNode('1', 'a', 'b')
+        with self.assertRaises(AssertionError):
+            LeafNode(1, 'a')
+        with self.assertRaises(AssertionError):
+            LeafNode('1', 'a', [1])
+        with self.assertRaises(AssertionError):
+            LeafNode('1', None)
+        with self.assertRaises(ValueError):
+            bad_leaf = LeafNode('1', '2')
+            bad_leaf.value = None
+            bad_html = bad_leaf.to_html() 
+            print(bad_html) # Get's the linter to shut the fuck up
+
+    def test_props_to_html(self):
+
+        self.assertEqual(self.node.props_to_html(), ' k1="v1" k2="v2"')
+        node2 = LeafNode("a", "b", props={})
+        self.assertEqual(node2.props_to_html(), "")
+
+
+    def test_repr(self):
+        text = f'{self.node}'
+        # print(text)
+        self.assertIn("value =", text)
+        self.assertNotIn("children:", text)
+        self.assertIn("tag =", text)
+        self.assertIn("props:", text)
+
+        min_node = LeafNode('a', 'b')
+        self.assertIn("props = None", str(min_node))
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
