6268948577a69df7e2dc1616600fc151ae554087
Brett Mattas <bmattas2@gmail.com>
2026-04-27 22:18:50 -0500

    Added code block parsing

--- src/block.py
+++ src/block.py
@@ -1,17 +1,92 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+def block_to_block_type(block):
+    # Returns the BlockType enum
+    # Block is a single string that can include multiple lines
+    
+    
+    if not isinstance(block, str):
+        raise ValueError("block_to_block_type needs a string input")
+    if len(block) < 1:
+        raise ValueError("block_to_block_type can't have an empty string")
+    if "\n\n" in block:
+        raise ValueError("block_to_block_type can't have double blank lines")
+        
+    lines = block.split("\n")
+        
+    if len(lines) < 1:
+        raise ValueError("Block needs at least one line")
+
+    # Check the first character to see if it fits any of these
+    # and avoid longer test
+    start_characters = "#`>-123456789"
+    # print(block[0][0])
+    if lines[0][0] not in start_characters:
+        # print("missing first char")
+        return BlockType.PARAGRAPH
+
+    # Heading starts with 1-6 # then a space
+    # print("heading regex:")
+    # print(re.findall(r"#{1,6}\s.*", lines[0]))
+    # print(f"{lines[0]=}")
+    if len(re.findall(r"^#{1,6}\s.*", lines[0])) > 0:
+        return BlockType.HEADING
+    
+    # Code starts with ``` then a new line and ends with ```
+    if (len(lines) >= 2 and 
+          lines[0][:3] == "```" and 
+          lines[-1][-3:] == "```"):
+        return BlockType.CODE
+    
+    # Quotes every line starts with >
+    if len([line for line in lines if line[0] == ">"]) == len(lines):
+        # print(block[0][0])
+        return BlockType.QUOTE
+    
+    # Unordered lists all start with - then a space
+    if len([line for line in lines if line[:2] == "- "]) == len(lines):
+        return BlockType.UNORDERED_LIST
+    
+    # Ordered lists all start with 1. and need to go up every line
+    is_ordered_list = True
+    digit = "1"
+    for line in lines:
+        if line[0:len(digit)+2] != digit+". ":
+            is_ordered_list = False
+            break
+        digit = str(int(digit)+1)
+
+    if is_ordered_list:
+        return BlockType.ORDERED_LIST
+            
+    # Default
+    return BlockType.PARAGRAPH
+
 
 def markdown_to_blocks(markdown):
 
     if len(markdown) < 1:
         return []
 
-    ret = markdown.split('\n\n')
+    ret = markdown.split("\n\n")
     ret = [b.strip() for b in ret]
     ret = [b for b in ret if len(b) > 0]
     return ret
 
+
 if __name__ == "__main__":
     print("inside block.py")
-    
+
     md = """
 This is **bolded** paragraph
 
@@ -22,10 +97,25 @@ This is the same paragraph on a new line
 - with items
 """
     blocks = markdown_to_blocks(md)
-    
+
     for block in blocks:
         print("\n")
         print(block)
-        
+
     print("\n")
     print(f"len(blocks)={len(blocks)}")
+
+    print("\n\n----------BLOCK TO BLOCK TYPE-----------")
+    
+    md = """
+> Quote 1
+> Quote 2
+"""
+    blocks = markdown_to_blocks(md)
+    print("blocks:")
+    print(f"len:{len(blocks)}")
+    for block in blocks:
+        print(block)
+        print(block_to_block_type(block))
+        print("\n")
+
--- src/test_block.py
+++ src/test_block.py
@@ -1,5 +1,5 @@
 import unittest
-from block import markdown_to_blocks
+from block import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestTextNode(unittest.TestCase):
         
@@ -46,3 +46,342 @@ And another block\n\n    Block with spaces at the beginning
                     "Block with spaces at the beginning"
                     ]
                 )
+    
+    def test_paragraph(self):
+        md = "Paragraph 01"
+        blocks = markdown_to_blocks(md)
+        result = []
+        for block in blocks:
+            result.append(block_to_block_type(block))
+        expected = [BlockType.PARAGRAPH]
+        self.assertEqual(result, expected)
+        
+        md = """
+Some text with spaces at the end     
+
+And another block\n\n    Block with spaces at the beginning
+"""
+        blocks = markdown_to_blocks(md)
+        result = []
+        for block in blocks:
+            result.append(block_to_block_type(block))
+        expected = [BlockType.PARAGRAPH,
+                    BlockType.PARAGRAPH,
+                    BlockType.PARAGRAPH]
+        self.assertEqual(result, expected)
+        
+        md = "1 Not a numbered list"
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.PARAGRAPH]
+        self.assertEqual(result, expected)
+        
+        md = "```Not code if doesn't end right``"
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.PARAGRAPH]
+        self.assertEqual(result, expected)
+        
+
+        md = "1.Numbered lists need spaces"
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.PARAGRAPH]
+        self.assertEqual(result, expected)
+        
+        md = "1 Numbered lists need periods"
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.PARAGRAPH]
+        self.assertEqual(result, expected)
+        
+        md = """
+> This would be a quotes block
+but every line needs >
+        """
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.PARAGRAPH]
+        self.assertEqual(result, expected)
+        
+        md = """
+- Should only count as an unordered list
+-If all lines conform
+"""
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.PARAGRAPH]
+        self.assertEqual(result, expected)
+        
+        
+    def test_block_type_error(self):
+
+        # Check that some are errors
+        md = """Paragraph 01
+
+
+        Paragraph 02
+        """
+        with self.assertRaises(ValueError):
+            # Should be split before being passed
+            print(block_to_block_type(md))
+            
+            
+        with self.assertRaises(ValueError):
+            # Need to pass strings
+            print(block_to_block_type(2))
+            
+        with self.assertRaises(ValueError):
+            # Cannot pass empty strings
+            print(block_to_block_type(""))
+        
+        bad_blocks = ["first ok", 1]
+
+        with self.assertRaises(ValueError):
+            # Cannot pass a list
+            print(block_to_block_type(bad_blocks))
+            
+        with self.assertRaises(ValueError):
+            # Cannot pass a list
+            print(block_to_block_type([]))
+    
+    def test_headings(self):
+
+        md = "# Heading 1"
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.HEADING]
+        self.assertEqual(result, expected)
+        
+        md = """
+# Heading 1
+
+## Heading 2
+
+### Heading 3
+
+#### Heading 4
+
+##### Heading 5
+
+###### Heading 6
+
+####### Not a heading
+"""
+        
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.HEADING,
+                    BlockType.HEADING,
+                    BlockType.HEADING,
+                    BlockType.HEADING,
+                    BlockType.HEADING,
+                    BlockType.HEADING,
+                    BlockType.PARAGRAPH,
+                    ]
+        self.assertEqual(result, expected)
+        
+        md = """
+# Heading
+I think everything is a heading if the first line is.
+"""
+        
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.HEADING]
+        self.assertEqual(result, expected)
+        
+    def test_code_block(self):
+        
+        md = """
+        ```
+        Some code
+        ```
+        """
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.CODE]
+        self.assertEqual(result, expected)
+        
+        md = """
+        ```
+        Still code```
+        """
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.CODE]
+        self.assertEqual(result, expected)
+        
+        md = """
+        ```
+        Still code
+        And then
+        some more code
+        Should ignore
+        ```
+        
+        """
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.CODE]
+        self.assertEqual(result, expected)
+        
+        md = """
+        ```
+        Still code
+        > Should still call
+        1. All of this code
+        - Always```
+        """
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.CODE]
+        self.assertEqual(result, expected)
+        
+    def test_quotes(self):
+
+        md = """
+        > Quote block
+        """
+
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.QUOTE]
+        # self.assertEqual(result, expected)
+        
+        md = """
+> Quote 1
+
+> Quote 2
+        """
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.QUOTE, BlockType.QUOTE]
+        # self.assertEqual(result, expected)
+
+        md = """
+> Quote 1
+> Quote 2
+        """
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.QUOTE]
+        self.assertEqual(result, expected)
+        
+        md = """
+> Quote 1
+> Quote 2
+
+Paragraph
+        """
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.QUOTE, BlockType.PARAGRAPH]
+        self.assertEqual(result, expected)
+        
+    def test_unordered(self):
+
+        md = "- Simple unordered"
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.UNORDERED_LIST]
+        self.assertEqual(result, expected)
+        
+        md = "- Two\n\n- Unordered"
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.UNORDERED_LIST, BlockType.UNORDERED_LIST]
+        self.assertEqual(result, expected)
+        
+        md = "- Two\n- Unordered"
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.UNORDERED_LIST]
+        self.assertEqual(result, expected)
+        
+        md = "- Two\n\n- Unordered\n\n-paragraph"
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.UNORDERED_LIST, BlockType.UNORDERED_LIST, BlockType.PARAGRAPH]
+        self.assertEqual(result, expected)
+        
+    
+    def test_ordered(self):
+
+        md = "1. Simple ordered"
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.ORDERED_LIST]
+        self.assertEqual(result, expected)
+        
+        md = "1. Two\n\n2. Unordered"
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.ORDERED_LIST, BlockType.PARAGRAPH]
+        self.assertEqual(result, expected)
+        
+        md = "1. Two\n2. Unordered"
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.ORDERED_LIST]
+        self.assertEqual(result, expected)
+        
+        md = "1. Two\n\n2. Unordered\n\n1. paragraph"
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.ORDERED_LIST, BlockType.PARAGRAPH, BlockType.ORDERED_LIST]
+        self.assertEqual(result, expected)
+
+    def test_combined(self):
+
+        
+        md = """
+This is a paragraph block
+
+# This is a heading
+
+1. Numbered list
+
+> Quote block
+
+- Unordered list
+
+```
+Code block```
+"""
+    
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.PARAGRAPH,
+                    BlockType.HEADING,
+                    BlockType.ORDERED_LIST,
+                    BlockType.QUOTE,
+                    BlockType.UNORDERED_LIST,
+                    BlockType.CODE]
+        self.assertEqual(result, expected)
+        
+        md = """
+```
+First code block
+```
+
+```
+Second code block
+```
+
+1. First
+2. Second
+3. Third
+
+###### Maximum heading!
+
+> Bigger
+> Quote
+
+- Unorder
+- will
+- Reign
+
+1. This
+> Is
+- Actually a paragraph
+
+Another paragraph
+
+```
+third code
+block
+```
+
+# Final heading
+"""
+
+
+        result = [block_to_block_type(block) for block in markdown_to_blocks(md)]
+        expected = [BlockType.CODE,
+                    BlockType.CODE,
+                    BlockType.ORDERED_LIST,
+                    BlockType.HEADING,
+                    BlockType.QUOTE,
+                    BlockType.UNORDERED_LIST,
+                    BlockType.PARAGRAPH,
+                    BlockType.PARAGRAPH,
+                    BlockType.CODE,
+                    BlockType.HEADING,
+                    ]
+        self.assertEqual(result, expected)
+
